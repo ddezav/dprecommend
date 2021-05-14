74c870e3ea1660619b8457685e500fdbf2acf2c0
suling <sulingv19@gmail.com>
2021-05-14 11:15:00 -0500

    knn content

--- ContentKNNAlgorithm.py
+++ ContentKNNAlgorithm.py
@@ -18,60 +18,44 @@ class ContentKNNAlgorithm(AlgoBase):
     def __init__(self, k=10, sim_options={}):
         AlgoBase.__init__(self)
         self.k = k
-        self.w2v= KeyedVectors.load_word2vec_format("modelowithpretrainded3.vec",binary=False)
-
+        #create the file csv
+        #get array from file csv 
+        #id_oa | latent_X | number of cluster 
+        self.oas= np.array([])
+        with open('/content/drive/MyDrive/elors_data/oalatentclusterid.csv') as f:
+            lines=f.readlines()
+            for line in lines:
+                myarray = np.fromstring(line, dtype=float, sep=',')
+                self.oas = np.append(self.oas, np.array(myarray), axis=0)
 
     def fit(self,trainset):
         AlgoBase.fit(self,trainset)
         ml = MovieLens()
-        df_oas = ml.getKeyWords()
 
         self.similarities = np.zeros((trainset.n_items,trainset.n_items))
         for i in range(self.trainset.n_items):
             for j in range(i+1,self.trainset.n_items):
                 idmovie1 = int(self.trainset.to_raw_iid(i))
                 idmovie2 = int(self.trainset.to_raw_iid(j))
-                self.similarities[i][j] = self.computeSimilarity(idmovie1,idmovie2,df_oas)
+                self.similarities[i][j] = self.computeSimilarity(idmovie1,idmovie2)
                 self.similarities[j][i] = self.similarities[i][j]
 
         np.savetxt("foo.csv", self.similarities, delimiter=",")
         return self
     
-    def normalize(s):
-        replace = (("á","a"),
-            ("é","e"),
-            ("í","i"),
-            ("ó","o"),
-            ("ú","u"),
-            ("ñ","n"),
-            ("Á","A"),
-            ("É","E"),
-            ("Í","I"),
-            ("Ó","O"),
-            ("Ú","U"),
-            ("Ñ","N"))
-        for a, b in replace:
-            s= s.replace(a,b).replace(a.upper(),b.upper())
-        return s
+    def computeSimilarity(self,idoa1,idoa2):
+        row_oa1 = self.oas[self.oas[:,0]==idoa1]
 
-    def computeSimilarity(self,idoa1,idoa2,df_oas):
-        #GET KEYWORDS FROM IDOA1
-        kw_oa1 = df_oas.iloc[idoa1,7]
-        list_kw_oa1 = ContentKNNAlgorithm.normalize(str(kw_oa1)).lower().split(",")
-        list_kw_oa1 = (' '.join(list_kw_oa1)).split(" ")
-        #GET KEYWORDS FROM IDOA2
-        kw_oa2 = df_oas.iloc[idoa2,7]
-        list_kw_oa2 = ContentKNNAlgorithm.normalize(str(kw_oa2)).lower().split(",")
-        list_kw_oa2 = (' '.join(list_kw_oa2)).split(" ")
+        row_oa2 = self.oas[self.oas[:,0]==idoa2]
+
+        #GET LATENT_X FROM IDOA1
+        latent_oa1 = row_oa1[:,1:-1]
+        #GET LATENT_X FROM IDOA2
+        latent_oa2 = row_oa2[:,1:-1]
         
-        result = 0.0
-
-        for i in range(len(list_kw_oa1)):
-            for j in range(len(list_kw_oa2)):
-                try:
-                    result+=(self.w2v.similarity(list_kw_oa1[i], list_kw_oa2[j]))
-                except:
-                    continue
+        result = len(latent_oa1)+len(latent_oa2)
+                  
+        #apply cosine metric or another
         return result        
 
 
@@ -82,9 +66,13 @@ class ContentKNNAlgorithm(AlgoBase):
         
         # Build up similarity scores between this item and everything the user rated
         neighbors = []
+    
         for rating in self.trainset.ur[u]:
-            genreSimilarity = self.similarities[i,rating[0]]
-            neighbors.append( (genreSimilarity, rating[1]) )
+            #get "m" oas that belongs to cluster of oa "i"
+            similar_oas = self.oas.getByCluster(i.getCluster(),m=5)
+            for similar_idoa in similar_oas:
+                similitud_oas = self.similarities[similar_idoa,rating[0]]
+                neighbors.append( (similitud_oas, rating[1]) )
         
         # Extract the top-K most-similar ratings
         k_neighbors = heapq.nlargest(self.k, neighbors, key=lambda t: t[0])
