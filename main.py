dc24a532471979a0cc8a85bd678960660bb2f398
suling <sulingv19@gmail.com>
2021-05-15 11:43:11 -0500

    get metrics from recommend system

---
+++ main.py
@@ -0,0 +1,38 @@
+from MovieLens import MovieLens
+from ContentKNNAlgorithm import ContentKNNAlgorithm
+from Evaluator import Evaluator
+from surprise import NormalPredictor
+
+import random
+import numpy as np
+def LoadMovieLensData():
+    ml = MovieLens()
+    print("Loading movie ratings...")
+    data = ml.loadMovieLensLatestSmall()
+    print("\nComputing movie popularity ranks so we can measure novelty later...")
+    rankings = ml.getPopularityRanks()
+    return (ml, data, rankings)
+
+np.random.seed(0)
+random.seed(0)
+
+# Load up common data set for the recommender algorithms
+(ml, evaluationData, rankings) = LoadMovieLensData()
+# Construct an Evaluator to, you know, evaluate them
+evaluator = Evaluator(evaluationData, rankings)
+
+oas= np.empty((0,102))
+with open('./oalatentclusterid.csv') as f:
+    lines=f.readlines()
+    for line in lines:
+        myarray = np.fromstring(line, dtype=float, sep=',')
+        a = myarray.reshape(1,102)
+        oas = np.append(oas, a, axis=0)
+contentKNN = ContentKNNAlgorithm(oas)
+evaluator.AddAlgorithm(contentKNN, "ContentKNN")
+
+# Just make random recommendations
+Random = NormalPredictor()
+evaluator.AddAlgorithm(Random, "Random")
+
+evaluator.Evaluate(True)
